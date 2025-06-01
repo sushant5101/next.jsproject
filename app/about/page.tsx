e1a61d27bdca72393dfa34a13b1c3908c395455f
sushant <aarosh27@gmail.com>
2025-06-01 13:06:59 +0545

    resolved the deploy errors 3rd time

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,13 @@ export default function About() {
         <div className="m-3">
             <h1 className="font-bold">About my Webpage</h1><br />
             <div className="w-[70%] p-1 border-white-300 rounded-md inline-block">
-                <p className="font-italic"><b><i>Welcome to my practice project!</i></b>. I am learning next.js to make websites fast and reliable. my existing webpage featuring a small game hang man yet remaining to be complete " <a className="hover:text-blue-400 hover:underline font-medium text-lg" target="blank" href="https://puzzleman-3ce04.web.app/"> https://puzzleman-3ce04.web.app/ </a> " (It will take you to login page it will take time if want to register or login.you can login via Username:<b> guest </b>, Password: <b>guest123</b> ).</p><br />
+                <p className="font-italic">
+                    <b><i>Welcome to my practice project!</i></b>. I am learning next.js to make websites fast and reliable.
+                    my existing webpage featuring a small game hang man yet remaining to be complete
+                    &quot;<a className="hover:text-blue-400 hover:underline font-medium text-lg" target="blank" href="https://puzzleman-3ce04.web.app/"> https://puzzleman-3ce04.web.app/ </a>&quot;
+                    (It will take you to login page it will take time if want to register or login.
+                    you can login via Username:<b> guest </b>, Password: <b>guest123</b> ).
+                </p><br />
 
                 <p>This website is made during my learning environment where I am actively exploring and using key concepts of web development using Next.js, React, and Tailwind CSS. My aim with this project is to increase my understanding of these frameworks use in practical application from what I{'\''}ve learned.</p>
 
